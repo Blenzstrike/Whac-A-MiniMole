7621fd70cedea5ae18d3f42b5c6fdcda0a71c271
Nick Hartmann <nick.hartmann@hotmail.com>
2024-06-14 21:19:57 +0200

    Improve comments for difficulty classes

--- Whac-A-MiniMole/Assets/Scripts/Difficulty/DifficultyDropdown.cs
+++ Whac-A-MiniMole/Assets/Scripts/Difficulty/DifficultyDropdown.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -23,14 +22,17 @@ public class DifficultyDropdown : MonoBehaviour
         //Clear already existing options
         difficultyTMPDropdown.ClearOptions();
 
-        List<OptionData> _newOptions = new List<OptionData>();
-        
+        //Create a list of option data and add the difficulties to that list.
+        List<OptionData> _newOptions = new List<OptionData>();        
         foreach (DifficultyClass difficultyClass in difficultyClasses) { 
             _newOptions.Add(new OptionData(difficultyClass.Name, difficultyClass.OptionImage));
         }
 
+        //Add options to the dropdown and subscribe to the listener.
         difficultyTMPDropdown.AddOptions(_newOptions);
         difficultyTMPDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+       
+        //Make sure the dropdown shows selected difficulty (or if there is no previously selected difficulty then show first difficulty)
         if (PlayerInformation.SelectedDifficulty != null)
         {
             for (int i = 0; i < difficultyClasses.Count; i++)
@@ -48,12 +50,21 @@ public class DifficultyDropdown : MonoBehaviour
         }
     }
 
-    private void OnDropdownValueChanged(int _index)
+    /// <summary>
+    /// Function that is called on the value change of the dropdown. Makes sure the selected difficulty is saved. 
+    /// </summary>
+    /// <param name="pIndex"></param>
+    private void OnDropdownValueChanged(int pIndex)
     {
-        if(_index > difficultyClasses.Count) { Debug.LogError("Value selected bigger than difficulty classes list"); }
-        CurrentDifficultyClass = difficultyClasses[_index];
+        if(pIndex > difficultyClasses.Count) { Debug.LogError("Value selected bigger than difficulty classes list"); }
+        CurrentDifficultyClass = difficultyClasses[pIndex];
     }
 
+    /// <summary>
+    /// Search through all difficulties and returns the difficulty class based on the name given. 
+    /// </summary>
+    /// <param name="pName">The name of the difficulty class that you look for</param>
+    /// <returns>Either the difficulty class with the specific name or null if there is no difficulty class with specific name</returns>
     public DifficultyClass GetDifficultyClassOnName(string pName)
     {
         foreach (DifficultyClass _difficultyClass in difficultyClasses)
